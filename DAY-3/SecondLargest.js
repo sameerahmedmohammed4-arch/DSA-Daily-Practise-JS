8aab4584529c6522b2ed52a412e2fcfac100de77
Mohammed Sameer Ahmed <sameerahmedmohammed4@gmail.com>
2026-07-23 15:19:53 +0530

    Some codes for DSA today...

--- DAY-3/SecondLargest.js
+++ DAY-3/SecondLargest.js
@@ -2,50 +2,50 @@
 
 // Method 01 - for sorted array
 
-let array1 = [0, 1, 2, 3, 6, 2, 1, 7, 42,4,5,7,3,32,56];
+let array1 = [0, 1, 2, 3, 6, 2, 7, 42,4,5,7,3,32,56,56];
 array1.sort((a,b) => (a-b));
 console.log(array1[array1.length -2]);
 
 
 // Method 02 - for unsorted array
 
-let array2 = [0, 3,4,6,1,7,20,4,2,7,9,5,5,];
+let array2 = [0,3,4,6,1,7,20,4,2,7,9,5,5,9,10];
 let max = 0;
-for (let  i =0; i<=array2.length-1;i++){
-    if (array2[i] > max){
+for(let i =0; i < array2.length; i++){
+    if(array2[i] > max){
         max = array2[i];
     }
 }
-let maxIndex = array2.indexOf(max);
-array2.splice(maxIndex,1)
-console.log(array2);
 
+let maxIndex = array2.indexOf(max)
+array2.splice(maxIndex, 1);
 let secondMax = 0;
-for(let j = 0;j<=array2.length -1 ;j++){
-    if (array2[j] > secondMax){
+for(let j =0; j < array2.length; j++){
+    if(array2[j] > secondMax){
         secondMax = array2[j];
     }
 }
-console.log("The second largest element is : ", secondMax);  // 9
+
+console.log("The second largest element is : ", secondMax);  // 10
 
 
 // Method 03 - for unsorted array
 
-let arr = [0, 3, 4, 6, 1, 7, 20, 4, 2, 7, 9, 5, 5];
+let arr = [0,3,2,1,4];
 
 let largest = -Infinity;
 let secondLargest = -Infinity;
-console.log(0 > -Infinity);
+// console.log(0 > -Infinity);
 for (let i = 0; i < arr.length; i++) {
-
-    if (arr[i] > largest) {
-        secondLargest = largest;  // 9
-        largest = arr[i]; // 20
-    }                                     
+          // 4      3  
+    if (arr[i] > largest) { // T
+        secondLargest = largest;  // 3
+        largest = arr[i]; // 4
+    }
     else if (arr[i] > secondLargest && arr[i] !== largest) {
-        secondLargest = arr[i];
+        secondLargest = arr[i]; //1
     }
 }
 
 console.log("Largest:", largest);
-console.log("Second Largest:", secondLargest);;
+console.log("Second Largest:", secondLargest);

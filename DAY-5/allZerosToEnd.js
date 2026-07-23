e47c6360d3325d078686b55879548210c05cc9a0
Mohammed Sameer Ahmed <sameerahmedmohammed4@gmail.com>
2026-07-23 22:16:24 +0530

    Created codes for moving all zeroes to end and Finding Index without Index value.

--- DAY-5/allZerosToEnd.js
+++ DAY-5/allZerosToEnd.js
@@ -1,19 +1,7 @@
 
 // Move All Zeros to the End
 
-// let arr = [0,3,0,5,2,4,0];
-// let count = 0;
-// for (let i = 0; i<arr.length; i++){
-//     if(arr[i] === 0){
-//         count++;
-//         arr.splice(i,1);
-//        arr.push(0)
-// }
-// }
-// console.log(arr);
 
-
-         //0  1  2  3  4  5  6  7  8
 let arr = [1, 0, 5, 0];
 
 let j = 0; //2
@@ -28,7 +16,25 @@ for (let i = 0; i < arr.length; i++){
         j++;
     }
 }
-
 console.log(arr);
 
 // Time complexity : O(n) , Space complexity : O(1)
+
+// ________________________________________________________________________________________________________________
+
+//Moving all zeroes to start
+
+let arr = [1, 0, 5, 0];
+
+let j = 0;
+
+for (let i = 0; i < arr.length; i++){
+         // 0 F
+    if (arr[i] === 0) {
+
+        [arr[i], arr[j]] = [arr[j], arr[i]];    
+
+        j++;
+    }
+}
+console.log(arr);
